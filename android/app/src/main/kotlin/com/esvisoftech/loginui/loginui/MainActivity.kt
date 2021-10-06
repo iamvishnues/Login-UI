77e0a83711e63ff427dae29210dba315929a993e
Vishnu ES <55495318+Vishnu-es@users.noreply.github.com>
2021-10-06 22:56:12 +0530

    splash screen

---
+++ android/app/src/main/kotlin/com/esvisoftech/loginui/loginui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.esvisoftech.loginui.loginui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(const myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
